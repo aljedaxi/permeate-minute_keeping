b7de3ed69b9699f34a5ce27cd659a79c8c9e4e9c
daxi <aljedaxi@gmail.com>
2019-06-13 07:26:18 -0600

    added yaml compilation functionality and error checking

--- filler.py
+++ filler.py
@@ -1,8 +1,9 @@
 import	jinja2 
 import  yaml
 from	jinja2		import Template, Environment
-from	subprocess	import call, Popen, PIPE, STDOUT
+from	subprocess	import call, check_call, CalledProcessError, Popen, PIPE, STDOUT
 from 	os			import path
+from    yml_to_tex.yml_to_tex import data_to_tex
 
 #template_file = ""
 
@@ -65,16 +66,15 @@ def fill(template_file, outfile, meeting):
         )
     )
 
-    call(("latexmk", "--pdf", outfile))
+    try:
+        check_call(("latexmk", "--pdf", outfile))
+    except CalledProcessError as e:
+        return 1
+    else:
+        return 0
 
 if __name__ == "__main__":
     from sys import argv
-
-    #particular = []
-    #while len(argv) > 1:
-    #    particular.append(argv.pop())
-    #if len(particular) is 0:
-    #    exit()
 
     particular = argv[1::]
     TEMPLATE = "template.tex"
@@ -82,13 +82,24 @@ if __name__ == "__main__":
 
     #for each argument
     for minutes in particular:
+        meeting = open(minutes).read()
+
+        #see if the meeting minutes were written in yml (a hobby of mine)
+        try:
+            text = yaml.load(meeting)
+        except yaml.scanner.ScannerError as e:
+            print("ok this isn't yaml")
+        else:
+            meeting = data_to_tex(text)
+
         if "z" in particular:
             template_file = ZINE_TEMPLATE
         else:
             template_file = TEMPLATE
 
-        meeting = open(minutes).read()
         outfile = f"""{minutes.split(".")[0]}_n.tex"""
         title = outfile.split(".")[0]
-        fill(template_file, outfile, meeting)
-        cleanup(title, minutes)
+        #fill returns 1 if it fails
+        failed_p = fill(template_file, outfile, meeting)
+        if not failed_p:
+            cleanup(title, minutes)
